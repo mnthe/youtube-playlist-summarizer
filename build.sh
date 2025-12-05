6e011788658ead4cbe2ca590a1235ec1f9d888e6
JungHun Kim <mnthe@pubg.com>
2025-12-04 23:14:28 -0800

    feat: 빌드 스크립트 추가 및 여러 YouTube 재생목록 업로드 기능 구현

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf-_r_unK-mfNSsvq1eEdNRO" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf8sqGuKk7yatvGdCbkIH_qM" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf9Ypc5vLyhoJObwzMc98K8R" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf-Gzj7psv0r9d1u9_yYAGus" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf_1rHZpRPA6MDUBv_OKz6Qk" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf9q8MC_iy2NGXaQiLhQgNdD" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf-a5wgEXBveQkE0MpHprsQt" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf_bh3ol1Obzb3P8n6BQENH9" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf8-QA1Hi5D-W2vRdc3h7FpK" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf8Kb_IJfMCw7CA630dbJL7a" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf9Vml7a6_rh4BNrLTTU0euv" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf8HawFRm2kL9935mQmLyGy1" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf9l1y_VZAm0vG2hproZMtip" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf_0uJ0iFTpJ6zhvGpSl-jsy" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf8fUGVeljVNpQwugV7XBZRt" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf-UqnINCmXu-dDZJm_B3bbJ" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf9t-nSD6dYTv-szvZxsBeh0" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf9m3loOlzEtdpobt-85B04t" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf8hzMmCGt2F--Oa6rF2rktF" &
+wait
---
+++ build2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf9MBvsGS5rB-X7CMvG21ib-" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf-yRZ2GBW1PJzz5cneMld9Z" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf_Chz_eS8KUdzzbuACSV0CE" &
+# npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf_1T4q-3gqy-PiZ57eAVONn" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf9iOBmyb15YPGkoMK1hAX_x" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf9BqAa6B07Xia-qmR23RHaJ" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf_LuBI1bIWPHQi88G6QR6KM" &
+npm run dev -- --locale ko --upload "https://krafton.atlassian.net/wiki/spaces/CEA/pages/820907634/re+Invent+2025" --playlist "https://www.youtube.com/playlist?list=PL2yQDdvlhXf-SZMaPAmK7huvDT6GBF18B" &
+wait
